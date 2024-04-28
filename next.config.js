1767fdde3161424b714f85270720918e5f869df2
Anish Chaulagain <anishchaulagain2058@gmail.com>
2024-04-28 13:44:46 +0545

    eslint disable

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
+    images: {
         domains: ['cdn.sanity.io']
     }
 }
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+
+}
